fcd7a914b739d0dec2a0ca36eb10af3a930b254c
Deon Botha <deon@oceanlabs.co>
2014-01-19 20:29:37 +0000

    Initial Commit

---
+++ OLImageEditor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OLImageEditor
+//
+//  Created by Deon Botha on 19/01/2014.
+//  Copyright (c) 2014 Deon Botha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OLImageEditor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OLImageEditor
+//
+//  Created by Deon Botha on 19/01/2014.
+//  Copyright (c) 2014 Deon Botha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
